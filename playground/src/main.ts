3ba8d312bbad4353901635c6c5e78b974e21f793
fatihblockgain <109206915+fatihblockgain@users.noreply.github.com>
2023-06-01 22:34:25 +0300

    feat: core package edited

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,48 +1,10 @@
 import "@unocss/reset/tailwind.css";
 import "virtual:uno.css";
 import "./style.css";
+import createMatter from "scrollmate";
 
-type Element = HTMLElement | null;
+const element = document.getElementById("app");
 
-const element: Element = document.getElementById("app");
+const matter = createMatter(element)
 
-function createMatter(el: Element) {
-  const scrollAreaStyle: Partial<CSSStyleDeclaration> = {
-    overflow: "hidden",
-  };
-
-  const getEl = (): Element => el;
-
-  const withNullCheck = (fn: Function) => {
-    return function (this: any, ...args: any[]) {
-      const targetEl = getEl();
-      if (!targetEl) {
-        console.warn("Element is null.");
-        return;
-      }
-      fn.apply(this, [targetEl, ...args]);
-    };
-  };
-
-  function assignStyles(
-    target: CSSStyleDeclaration | undefined,
-    styles: Partial<CSSStyleDeclaration>
-  ) {
-    if (!target) return;
-    Object.assign(target, styles);
-  }
-
-  const scrollArea = withNullCheck(function (): void {
-    const element = getEl()!;
-    const content = element.innerHTML;
-    assignStyles(element.style, scrollAreaStyle);
-    element.innerHTML = `<div class="scrollmater-content">${content}</div>`;
-  });
-
-  return {
-    scrollArea,
-  };
-}
-
-const matter = createMatter(element);
 matter.scrollArea();
